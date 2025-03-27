8147aa0884b666b16f4e8998523c8161d570f683
denistec <yodygranadosrojas@hotmail.com>
2025-03-26 21:18:51 -0500

    agregué tarea 3 y 4

--- sd-l1-1.0/task4.js
+++ sd-l1-1.0/task4.js
@@ -3,3 +3,29 @@
 for (let i = 0; i < 1; i++) {
     console.log("This is Task Four!");
   };
+
+  for (let i = 1; i <= 105; i++) {
+    if (i % 7 == 0 && i % 5 == 0 && i % 3 == 0) {
+      console.log("FizzBuzzWoof"); 
+    }
+    else if (i % 7 == 0 && i % 5 == 0) {
+      console.log("FizzWoof");
+    }
+    else if (i % 7 == 0 && i % 3){
+      console.log("BuzzWoof");
+    }
+    else if (i % 3 == 0 && i % 5 == 0) {
+      console.log("Fizz")
+    }
+    else if  (i % 3 == 0) {
+      console.log("Fizz");
+    }
+    else if (i % 5 == 0) {
+      console.log("Buzz")
+    }  
+    else if ( i % 7 == 0) {
+      console.log("Woof");
+      
+    }
+    else {console.log(i);}
+  }
